de2caa4f063a0d9f309fde6cef702c583c54a3ce
unknown <patelvidhi8420@gmail.com>
2026-06-13 18:50:03 +0530

    Add Render's domain in settings.py file

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)xf=%=&uvex%p62t2vbitvi&f4+_zx1fl48557k#78dt^d!5vo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fruitshop-jlbj.onrender.com']
 
 
 # Application definition
